41a01a47dba7c5a23f8354e11194b08cc5b3cde6
Jan Kozel <153216339+WorkLoopC@users.noreply.github.com>
2024-01-25 23:03:37 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,11 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
-int main(void){
-DDRB=(1<<PB0);
-while(1){
-PORTB=(1<<PB0);
+int main(){
+DDRB=0x01;
+while (1){
+PORTB=0x01;
 _delay_ms(500);
 PORTB=0;
 _delay_ms(500);
-
-}
+  }
 }
